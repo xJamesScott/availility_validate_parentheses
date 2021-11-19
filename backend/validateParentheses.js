80c82862816efbcef42fe5ed45d1f07509728b6d
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-11-18 23:37:46 -0500

    added parentheses validation

---
+++ backend/validateParentheses.js
@@ -0,0 +1,27 @@
+require('express');
+
+const string = "asdl()()()fkaj"
+
+const validateParentheses = () => {
+    let pCount = 0;
+
+    for (i in string) {
+        if (string[i] === "(") {
+            pCount++;
+        } else if (string[i] === ")") {
+            if (pCount == 0) {
+                console.log("NOT VALID")
+                return
+            } else {
+                pCount--;
+            }
+        }
+    }
+    if (pCount === 0) {
+        console.log("VALIDATED!")
+    } else {
+        console.log("NOT VALID")
+    }
+};
+
+module.exports = validateParentheses
