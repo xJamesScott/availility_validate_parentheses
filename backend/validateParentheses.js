43ab333eefc3222de7661449fe82335d5440e068
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-11-19 14:48:40 -0500

    updated README

--- backend/validateParentheses.js
+++ backend/validateParentheses.js
@@ -1,27 +1,27 @@
-require('express');
+require("express");
 
-const string = "asdl()()()fkaj"
+const string = "asdl()()()fkaj";
 
 const validateParentheses = () => {
-    let pCount = 0;
+  let pCount = 0;
 
-    for (i in string) {
-        if (string[i] === "(") {
-            pCount++;
-        } else if (string[i] === ")") {
-            if (pCount == 0) {
-                console.log("NOT VALID")
-                return
-            } else {
-                pCount--;
-            }
-        }
+  for (i in string) {
+    if (string[i] === "(") {
+      pCount++;
+    } else if (string[i] === ")") {
+      if (pCount == 0) {
+        console.log("NOT VALID");
+        return;
+      } else {
+        pCount--;
+      }
     }
-    if (pCount === 0) {
-        console.log("VALIDATED!")
-    } else {
-        console.log("NOT VALID")
-    }
+  }
+  if (pCount === 0) {
+    console.log("VALIDATED!");
+  } else {
+    console.log("NOT VALID");
+  }
 };
 
-module.exports = validateParentheses
+module.exports = validateParentheses;
